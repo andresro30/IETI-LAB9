d7b81314ec6f1699c261dfa71302cc66110a0710
andresro30 <andresrochag15@hotmail.com>
2020-10-29 10:24:00 -0500

    first part

---
+++ src/main/java/eci/ieti/data/model/User.java
@@ -0,0 +1,50 @@
+package eci.ieti.data.model;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s, name='%s', email='%s']",
+                id, name, email);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
